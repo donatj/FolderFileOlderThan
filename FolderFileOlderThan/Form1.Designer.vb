8d2c418c09eae32f11f2de7161b31620bcf21d17
Jesse Donat <donatj@gmail.com>
2011-05-20 14:59:35 -0500

    Init Commit

---
+++ FolderFileOlderThan/Form1.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.BtnBrowse = New System.Windows.Forms.Button()
+        Me.txtPath = New System.Windows.Forms.TextBox()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DateCombo = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(388, 72)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(88, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(13, 55)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(463, 11)
+        Me.ProgressBar2.TabIndex = 2
+        '
+        'BtnBrowse
+        '
+        Me.BtnBrowse.Location = New System.Drawing.Point(273, 12)
+        Me.BtnBrowse.Name = "BtnBrowse"
+        Me.BtnBrowse.Size = New System.Drawing.Size(75, 20)
+        Me.BtnBrowse.TabIndex = 4
+        Me.BtnBrowse.Text = "Browse"
+        Me.BtnBrowse.UseVisualStyleBackColor = True
+        '
+        'txtPath
+        '
+        Me.txtPath.Location = New System.Drawing.Point(13, 12)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.Size = New System.Drawing.Size(254, 20)
+        Me.txtPath.TabIndex = 5
+        '
+        'BackgroundWorker1
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(10, 77)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(12, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "x"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(13, 38)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(463, 11)
+        Me.ProgressBar1.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 96)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(12, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "y"
+        '
+        'DateCombo
+        '
+        Me.DateCombo.FormattingEnabled = True
+        Me.DateCombo.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "18", "24", "32", "64"})
+        Me.DateCombo.Location = New System.Drawing.Point(355, 11)
+        Me.DateCombo.Name = "DateCombo"
+        Me.DateCombo.Size = New System.Drawing.Size(74, 21)
+        Me.DateCombo.TabIndex = 9
+        Me.DateCombo.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(435, 15)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Months"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(492, 123)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.DateCombo)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPath)
+        Me.Controls.Add(Me.BtnBrowse)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Folder Date Scanner"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar2 As System.Windows.Forms.ProgressBar
+    Friend WithEvents BtnBrowse As System.Windows.Forms.Button
+    Friend WithEvents txtPath As System.Windows.Forms.TextBox
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents DateCombo As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ FolderFileOlderThan/Form1.vb
@@ -0,0 +1,179 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        txtPath.Text = My.Settings.lastPath
+        Label1.Text = ""
+        Label2.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        BackgroundWorker1.WorkerReportsProgress = True
+        BackgroundWorker1.RunWorkerAsync(DateCombo.Text)
+        Button1.Enabled = False
+        BtnBrowse.Enabled = False
+    End Sub
+
+
+    Private Sub BtnBrowse_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnBrowse.Click
+        Dim i As New FolderBrowserDialog
+
+        i.SelectedPath = txtPath.Text
+        i.ShowDialog()
+        txtPath.Text = i.SelectedPath
+        My.Settings.lastPath = i.SelectedPath
+        My.Settings.Save()
+    End Sub
+
+
+    Private Sub BackgroundWorker1_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Dim myDateBefore As Date = Date.Now
+        myDateBefore = myDateBefore.AddMonths(0 - e.Argument)
+        Dim theState As New myState
+
+        Dim folders As String() = IO.Directory.GetDirectories(txtPath.Text)
+        theState.max = folders.Count
+
+        Dim filename As String = My.Computer.FileSystem.SpecialDirectories.Desktop.ToString & "\FileDateOutput" & Date.Now.Ticks & ".csv"
+        Dim oWrite As New StreamWriter(filename)
+        oWrite.AutoFlush = True
+        oWrite.WriteLine("Path,Create,Modify,Access")
+
+        For Each folder As String In folders
+
+            Dim newestC As New Date(0)
+            Dim newestM As New Date(0)
+            Dim newestA As New Date(0)
+
+            theState.cur += 1
+            theState.cur2 = 0
+
+            theState.name = "Scanning: " & folder
+            BackgroundWorker1.ReportProgress(1, theState)
+
+            Dim files As String() = filescan(folder).ToArray
+
+            theState.name = folder
+            BackgroundWorker1.ReportProgress(1, theState)
+            theState.max2 = files.Count
+
+            Dim j As Integer = 0
+            For Each file As String In files
+
+                If True Then
+
+                    Dim fileCdate = IO.File.GetCreationTime(file)
+                    If newestC < fileCdate Then
+                        newestC = fileCdate
+                    End If
+
+                    Dim fileMdate = IO.File.GetLastWriteTime(file)
+                    If newestM < fileMdate Then
+                        newestM = fileMdate
+                    End If
+
+                    Dim fileAdate = IO.File.GetLastAccessTime(file)
+                    If newestA < fileAdate Then
+                        newestA = fileAdate
+                    End If
+
+                End If
+
+                j += 1
+                theState.cur2 = j
+                theState.total += 1
+                If (j Mod 10 = 0) Then
+                    BackgroundWorker1.ReportProgress(1, theState)
+                End If
+            Next
+
+            If newestM < myDateBefore Or e.Argument < 1 Then
+                oWrite.WriteLine("""" & folder & """" & "," & newestC & "," & newestM & "," & newestA)
+            End If
+
+        Next
+
+        oWrite.Close()
+
+        MsgBox("Done!")
+        BackgroundWorker1.ReportProgress(1, theState)
+
+    End Sub
+
+
+    Class myState
+        Public name As String
+        Public max As Integer
+        Public cur As Integer
+        Public max2 As Integer
+        Public cur2 As Integer
+        Public total As Integer
+    End Class
+
+
+    Private Sub BackgroundWorker1_ProgressChanged(ByVal sender As Object, ByVal e As System.ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        Label1.Text = e.UserState.name
+        Label2.Text = "Total Files Scanned: " & e.UserState.total
+        ProgressBar1.Maximum = e.UserState.max
+        ProgressBar1.Value = e.UserState.cur
+        ProgressBar2.Maximum = e.UserState.max2
+        ProgressBar2.Value = e.UserState.cur2
+    End Sub
+
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        Button1.Enabled = True
+        BtnBrowse.Enabled = True
+    End Sub
+
+
+    Private Sub DateCombo_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles DateCombo.TextChanged
+
+        Try
+            DateCombo.Text = Convert.ToInt16(DateCombo.Text)
+        Catch ex As Exception
+            DateCombo.Text = 0
+        End Try
+
+    End Sub
+
+    Function dirscan(ByVal path As String) As List(Of DirectoryInfo)
+        On Error Resume Next
+        Dim i As New DirectoryInfo(path)
+        Dim paths As New List(Of DirectoryInfo)
+        For Each directory As DirectoryInfo In i.GetDirectories()
+
+            If directory.Name.Chars(0) <> "." And directory.Name <> "html2pdf" And directory.Name <> "test" And directory.Name <> "cache" And directory.Name <> "ckeditor" And directory.Name <> "modules" Then
+                paths.Add(directory)
+                Dim j As List(Of DirectoryInfo) = dirscan(directory.FullName)
+                paths.AddRange(j)
+            End If
+
+        Next
+
+        Return paths
+    End Function
+
+    Function filescan(ByVal path As String) As List(Of String)
+        On Error Resume Next
+        Dim dirs As List(Of DirectoryInfo) = dirscan(path)
+        Dim paths As New List(Of String)
+
+        Dim l As New DirectoryInfo(path)
+        For Each j In l.GetFiles()
+            paths.Add(j.FullName)
+        Next
+
+        For Each dirr As DirectoryInfo In dirs
+
+            For Each j In dirr.GetFiles()
+                paths.Add(j.FullName)
+            Next
+
+        Next
+
+        Return paths
+    End Function
+
+End Class
